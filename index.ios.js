fcd25f54642316aa47bed073cb49e0de1fc0464e
Andrew Hird <andrew.james.hird@gmail.com>
2016-11-07 18:42:08 +1300

    background playing working

--- index.ios.js
+++ index.ios.js
@@ -1,37 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   Image,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
-} from 'react-native';
+} from 'react-native'
 
-import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming';
+import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming'
 
-const url = "http://streams.95bfm.com/stream95";
-ReactNativeAudioStreaming.pause();
-ReactNativeAudioStreaming.resume();
-ReactNativeAudioStreaming.play(url, {showIniOSMediaCenter: true, showInAndroidNotifications: true});
-ReactNativeAudioStreaming.stop();
+const url = "http://streams.95bfm.com/stream95"
+ReactNativeAudioStreaming.pause()
+ReactNativeAudioStreaming.resume()
+ReactNativeAudioStreaming.play(url, {showIniOSMediaCenter: true, showInAndroidNotifications: true})
+ReactNativeAudioStreaming.stop()
 
 export default class bFMPlayer extends Component {
   render() {
     return (
       <View style={styles.container}>
+      <TouchableHighlight onPress={() => { ReactNativeAudioStreaming.play(url, {showIniOSMediaCenter: true, showInAndroidNotifications: true})}}>
       <Image
-        source={require('./img/bfm.png')}
+        style={styles.button}
+        source={require('./img/bfm_full.png')}
       />
-        <Text style={styles.welcome}>
-          Click below to stream
-        </Text>
-        <Player url={url} />
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => { ReactNativeAudioStreaming.stop()}}>
+      <Text style={styles.welcome}>
+        Click here to stop
+      </Text>
+      </TouchableHighlight>
+        {/* <Player url={url} /> */}
       </View>
     );
   }
